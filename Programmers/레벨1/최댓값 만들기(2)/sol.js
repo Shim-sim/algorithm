86ee0e8c5a6ce5733653df2f92f09710575183cd
ShimSungBo <shimdb96@gmail.com>
2022-11-29 19:04:24 +0900

    feat:(프로그래머스) 최댓값 만들기2

---
+++ Programmers/레벨1/최댓값 만들기(2)/sol.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+  let BruteForce = [];
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = 0; j < numbers.length; j++) {
+      if (i !== j) {
+        BruteForce.push(numbers[i] * numbers[j]);
+      }
+    }
+  }
+
+  return Math.max(...BruteForce);
+}
