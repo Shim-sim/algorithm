0c8902cca56a0d89e0c2161ace57f31e7677a610
ShimSungBo <shimdb96@gmail.com>
2023-04-18 08:58:02 +0900

    재귀기초

--- 재귀/재귀기초문제.js
+++ 재귀/재귀기초문제.js
@@ -10,3 +10,22 @@ function factorial(num) {
   if (num === 0) return 1;
   return num * factorial(num - 1);
 }
+
+//productOfArray
+function productOfArray(arr) {
+  if (arr.length === 0) return 1;
+  return arr[0] * productOfArray(arr.slice(1));
+}
+
+// recursiveRange
+function recursiveRange(num) {
+  if (num === 1) return 1;
+  return num + recursiveRange(num - 1);
+}
+
+//fib
+function fib(x) {
+  if (x === 0) return 0;
+  else if (x === 1) return 1;
+  else return fib(x - 1) + fib(x - 2);
+}
