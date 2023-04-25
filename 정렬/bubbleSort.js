18af5d74865723b4537a06ebad52230a00d41c80
ShimSungBo <shimdb96@gmail.com>
2023-04-25 09:56:58 +0900

    버블정렬

---
+++ 정렬/bubbleSort.js
@@ -0,0 +1,19 @@
+function bubbleSort(arr) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx[2]]] = [arr[idx2], arr[idx1]];
+  };
+  let noSwaps;
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+  return arr;
+}
+
+// 복잡도 (On)
