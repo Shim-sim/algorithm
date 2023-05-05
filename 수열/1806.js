5e1f44d4384af9fc8414aa5f4812787abbf90738
Shim-sim <shimdb96@gmail.com>
2023-05-05 16:14:06 +0900

    (백준) 투포인터 1806.js

---
+++ 수열/1806.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const [N, S] = input[0].split(" ").map(Number);
+const arr = input[1].split(" ").map(Number);
+
+// 10,000 이하의 자연수로 이루어진 길이 N짜리 수열이 주어진다.
+//  이 수열에서 연속된 수들의 부분합 중에 그 합이 S 이상이 되는 것 중,
+//   가장 짧은 것의 길이를 구하는 프로그램을 작성하시오.
+let sum = 0;
+let minLength = Infinity;
+let i = 0;
+let j = 0;
+
+while (j < N) {
+  sum += arr[j];
+
+  while (sum >= S) {
+    sum -= arr[i];
+    minLength = Math.min(minLength, j - i + 1);
+    i++;
+  }
+
+  j++;
+}
+
+console.log(minLength === Infinity ? 0 : minLength);
