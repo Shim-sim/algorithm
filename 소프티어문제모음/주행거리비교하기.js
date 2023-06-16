c0db9fa0e2e1a80606b35368b8bedea9fe6b966a
ShimSungBo <shimdb96@gmail.com>
2023-06-16 10:04:32 +0900

    lv.1 주행거리비교하기

---
+++ 소프티어문제모음/주행거리비교하기.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const [A, B] = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split(' ')
+  .map(Number);
+
+console.log(A > B ? 'A' : A === B ? 'same' : 'B');
