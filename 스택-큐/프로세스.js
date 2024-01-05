b7baad4d1dc4a36657a593927b44a362cf34d25c
ShimSungBo <shimdb96@gmail.com>
2024-01-05 09:03:40 +0900

    프로세스 설명추가

--- 스택-큐/프로세스.js
+++ 스택-큐/프로세스.js
@@ -18,3 +18,9 @@ function solution(priorities, location) {
   }
   return answer;
 }
+
+// 틀릴 답안
+// 처음 문제를 접근할 때 '아 queue로 접근해서 현재 값보다 큰 값이 없으면 해당 값을 리턴해야겠다'
+// 라고 생각했는데 문제 자체를 잘못이해했다.
+// 결국에는 프로세스가 돌 때 location의 값의 index를 기억해줘야했다.
+// priorities를 array로 재배열 하만셔 index를 추가해줬고 나머지는 같은 로직으로 쉽게 해결할 수 있었다.
