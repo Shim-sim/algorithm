85ca0e29a10c0568d1381e134b2ff8e5894be3ca
ShimSungBo <shimdb96@gmail.com>
2024-01-18 10:37:55 +0900

    (백준) 이진탐색, 수열 풀이 추가

--- 수열/10816.js
+++ 수열/10816.js
@@ -15,3 +15,29 @@ let answer = [];
 M.forEach((item) => answer.push(map.get(item) || 0));
 
 console.log(answer.join(" "));
+
+// ---------------------------------------------------------------------
+
+const fs = require("fs");
+const fileSync = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs.readFileSync(fileSync).toString().trim().split("\n");
+
+const [n, number, m, card] = input;
+const answer = [];
+
+const newNumber = number.split(" ").map(Number);
+let newCard = card.split(" ").map(Number);
+
+// 두 번째 배열에서 각 원소의 개수를 미리 계산
+const countMap = {};
+for (const item of newNumber) {
+  countMap[item] = (countMap[item] || 0) + 1;
+}
+
+// 첫 번째 배열을 순회하면서 두 번째 배열과의 일치하는 원소 개수를 찾음
+for (const item of newCard) {
+  const count = countMap[item] || 0;
+  answer.push(count);
+}
+
+console.log(answer.join(" "));
--- 이진탐색.투포인터/이진탐색.js
+++ 이진탐색.투포인터/이진탐색.js
@@ -1,8 +1,8 @@
-let fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
-let input = fs.readFileSync(filePath).toString().split('\n');
+let fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
 let target = input.shift().split().map(Number)[0];
-const arr = input.shift().split(' ').map(Number);
+const arr = input.shift().split(" ").map(Number);
 
 let start = 0;
 let end = arr.length - 1;
@@ -10,9 +10,11 @@ let result = 0;
 while (start <= end) {
   let mid = Math.floor((start + end) / 2);
 
+  // 타겟이 작다 미드보다 그러면 mid를 기준으로 더 작은 값이다.
   if (target < arr[mid]) {
     end = mid - 1;
   } else {
+    // 타겟이 크다 미드보다 그러면 mid를 기준으로 더 큰 값이다.
     start = mid + 1;
     result = mid;
   }
