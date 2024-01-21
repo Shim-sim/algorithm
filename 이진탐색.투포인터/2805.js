80db65b6c3fc521bcde2b0baede7dcf0e9c7c761
ShimSungBo <shimdb96@gmail.com>
2024-01-21 16:08:39 +0900

    (백준) 나무자르기 재풀이

--- 이진탐색.투포인터/2805.js
+++ 이진탐색.투포인터/2805.js
@@ -1,8 +1,8 @@
-let fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
-let input = fs.readFileSync(filePath).toString().split('\n');
-const [n, target] = input.shift().split(' ').map(Number);
-const arr = input.shift().split(' ').map(Number);
+let fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+const [n, target] = input.shift().split(" ").map(Number);
+const arr = input.shift().split(" ").map(Number);
 
 let start = 0;
 let end = Math.max(...arr);
@@ -27,3 +27,7 @@ while (start <= end) {
 }
 
 console.log(result);
+
+// 문제푸는데 걸린 시간 15분
+// 딱히 어려움은 없었다 근데 height의 값을 언제 바꿔주는지에서 헷갈려서 시간이 조금 걸렸다.
+// 우선 total > m 이 되면 나무를 자르는데 필요한 최소값을 만족하기 때문에 height의 값을 바꿔주면 된다.
