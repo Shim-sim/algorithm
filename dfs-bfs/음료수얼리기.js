567b5706166a8548b5f6085fbfdf84bb26fa0bbe
Shim-sim <shimdb96@gmail.com>
2024-01-31 21:45:04 +0900

    dfs 문제 풀이 변경

--- dfs-bfs/음료수얼리기.js
+++ dfs-bfs/음료수얼리기.js
@@ -1,66 +1,45 @@
-function solution(input) {
-  let inputList = input.split("\n");
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-  let size = inputList[0].split(" ").map((x) => parseInt(x));
-  let n = size[0]; // 세로 y
-  let m = size[1]; // 가로 x
+const [n, m] = input.shift().split(" ").map(Number);
 
-  let iCount = 0; // 아이스크림 개수
-
-  let graph = new Array();
-  for (let i = 1; i <= n; i++) {
-    graph.push(inputList[i].split("").map((x) => parseInt(x)));
+const dfs = (x, y) => {
+  // 주어진 범위를 벗어나는 경우에는 즉시 종료
+  if (x <= -1 || x >= n || y <= -1 || y >= m) {
+    return false;
   }
 
-  for (let i = 0; i < m; i++) {
-    // x
-    for (let j = 0; j < n; j++) {
-      // y
-      // 1. 그래프가 0인지 확인
-      if (graph[j][i] === 0) {
-        // 상하좌우 돌면서 0이면 2로 변이
-        dfs(graph, i, j, m, n);
-        iCount++;
-      }
-    }
+  // 현재 노드를 아직 방문하지 않았다면
+  if (graph[x][y] === 0) {
+    // 해당 노드를 방문처리
+    graph[x][y] = 1;
+
+    // 상,하,좌,우 위치들도 모두 재귀적으로 호출
+    dfs(x - 1, y);
+    dfs(x, y - 1);
+    dfs(x + 1, y);
+    dfs(x, y + 1);
+    return true;
+  } else {
+    return false;
   }
+};
 
-  // 정답
-  console.log(iCount);
+const graph = new Array();
+let result = 0;
+input.forEach((item) => {
+  graph.push(item.split("").map(Number));
+});
 
-  return iCount;
-}
-
-function dfs(graph, curNodX, curNodY, maxX, maxY) {
-  let dir = [
-    [0, -1],
-    [1, 0],
-    [0, 1],
-    [-1, 0],
-  ]; // 탐색 범위 이해
-
-  // 1. 방문
-  graph[curNodY][curNodX] = 2;
-
-  // 2. 상우하좌 일일이 확인
-  for (let i = 0; i < dir.length; i++) {
-    // 3. 다음으로 갈 좌표 설정
-    let nX = curNodX + dir[i][0];
-    let nY = curNodY + dir[i][1];
-
-    // 3. 다음 좌표가 갈 수 있는 곳인지 확인
-    if (nX >= 0 && nY >= 0 && nX < maxX && nY < maxY) {
-      if (graph[nY][nX] === 0) {
-        dfs(graph, nX, nY, maxX, maxY);
-      }
-    }
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < m; j++) {
+    if (dfs(i, j)) result++;
   }
 }
 
-const test = `4 5
-00110
-00011
-11111
-00000`;
+console.log(result);
 
-solution(test);
+// 주어진 범위를 벗어나는 경우 즉시 종료 되는 이유
+// 상하좌우 모두 확인해야하기 때문에 matrix의 상하좌우 끝은 -1이 올 수 있다.
+// 그래서 주어진 범위에서 -1 || n이나 m이 넘었을 때 false로 return 해주어야한다.
