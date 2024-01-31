300dbca220eec3b33571880e78f28eab3cf54e7e
Shim-sim <shimdb96@gmail.com>
2024-01-31 22:05:40 +0900

    bfs: 미로탈출 재풀이

--- dfs-bfs/미로탈출.js
+++ dfs-bfs/미로탈출.js
@@ -1,20 +1,27 @@
-function solution(n, m, maze) {
-  const graph = [];
-  maze.split('\n').map((m) => {
-    m = m.split('').map(Number);
-    graph.push(m);
-  });
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-  // 상하좌우
-  dx = [-1, 1, 0, 0];
-  dy = [0, 0, -1, 1];
+const [n, m] = input.shift().split(" ").map(Number);
 
+const graph = new Array();
+let result = 0;
+input.forEach((item) => {
+  graph.push(item.split("").map(Number));
+});
+
+const dx = [-1, 1, 0, 0];
+const dy = [0, 0, -1, 1];
+
+const bfs = () => {
   const queue = [];
-  let x = 0,
-    y = 0;
+  let x = 0;
+  let y = 0;
+
   queue.push([x, y]);
 
   while (queue.length !== 0) {
+    console.log(queue);
     const temp = queue.shift();
     x = temp[0];
     y = temp[1];
@@ -26,7 +33,7 @@ function solution(n, m, maze) {
       // 범위를 벗어나지 않는지 체크
       if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
 
-      // 이동이 가능한 경로에 (현재 위치 + 1를 해준다.)
+      // 이동이 가능한 경로에 (현재 위치 + 1을 해준다.)
       // 그 다음 queue에는 해당 노드를 다시 push 해준다.
       if (graph[nx][ny] === 1) {
         graph[nx][ny] = graph[x][y] + 1;
@@ -34,8 +41,8 @@ function solution(n, m, maze) {
       }
     }
   }
-  // 2차원 배열의 크기를 생각하면 n-1, m-1이 맞다.
-  console.log(graph[n - 1][m - 1]);
-}
+  //index 0 부터 시작하기 때문에 -1의 위치의 값을 출력한다.
+  return graph[n - 1][m - 1];
+};
 
-solution(5, 6, '101010\n111111\n000001\n111111\n111111');
+console.log(bfs());
