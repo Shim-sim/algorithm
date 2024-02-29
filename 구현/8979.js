eba91874e7943dfc3ddb296571a6be6792d91bdd
ShimSungBo <shimdb96@gmail.com>
2024-02-29 09:23:46 +0900

    올림픽 문제 재풀이

--- 구현/8979.js
+++ 구현/8979.js
@@ -1,35 +1,32 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-let input = fs.readFileSync(filePath).toString().trim().split("\n");
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const [N, K] = input.shift().split(" ").map(Number);
-const countries = input.map((line) => line.split(" ").map(Number));
+const [n, target] = input.shift().split(" ").map(Number);
+const countries = input.map((item) => {
+  return item.split(" ").map(Number);
+});
 
-// 메달 순으로 국가를 정렬합니다.
+// 금메달이 같지 않으면 수행, 없으면 다음, 그래도 없으면 동메달 비교
 countries.sort((a, b) => {
-  if (a[1] !== b[1]) return b[1] - a[1]; // 금메달 수 비교
-  if (a[2] !== b[2]) return b[2] - a[2]; // 은메달 수 비교
+  if (a[1] !== b[1]) return b[1] - a[1]; // 금메달 수 비교 내림차순
+
+  if (a[2] !== b[2]) return b[2] - a[2]; // 은메달 수 비교 내림차순
   return b[3] - a[3]; // 동메달 수 비교
 });
 
-// K 국가의 메달 정보를 찾습니다.
-const KMedals = countries.find((country) => country[0] === Number(K));
+const KMedals = countries.find((item) => item[0] === target);
 
-// K 국가의 등수를 계산합니다.
 let rank = 1;
-for (let country of countries) {
-  if (
-    country[1] > KMedals[1] ||
-    (country[1] === KMedals[1] && country[2] > KMedals[2]) ||
-    (country[1] === KMedals[1] &&
-      country[2] === KMedals[2] &&
-      country[3] > KMedals[3])
+
+for (let item of countries) {
+  if (item[0] === target) break;
+  else if (
+    item[1] > KMedals[1] ||
+    (item[1] === KMedals[1] && item[2] > KMedals[2]) ||
+    (item[1] === KMedals[1] && item[2] === KMedals[2] && item[3] > KMedals[3])
   ) {
     rank++;
-
-    // 1등이면 바로 break
-  } else if (country[0] === K) {
-    break;
   }
 }
 
