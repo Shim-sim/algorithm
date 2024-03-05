c44fcc82f93d9906238e405e245572b6a4ef1423
ShimSungBo <shimdb96@gmail.com>
2024-03-05 09:54:48 +0900

    숫자문자열 array풀이추가

--- 문자열/숫자문자열과영단어.js
+++ 문자열/숫자문자열과영단어.js
@@ -18,6 +18,27 @@ function solution(s) {
   return Number(s);
 }
 
+// array 풀이
+function solution2(s) {
+  const number = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+  number.forEach((item, idx) => (s = s.replaceAll(item, idx)));
+  return Number(s);
+}
+// replace가 아닌 replaceAll을 사용해야 하는 이유
+// replace() 메서드는 해당 문자열을 한 번만 찾아 변경하고 replaceAll()은 해당하는 문자열을 모두 찾아 치환한다.
+
 // 문제푸는데 걸린 시간 25분
 // split하고 join을 어떤 기준으로 해야할지 애먹은 문제였다.
 // object가 아닌 array를 사용해서 문제를 해결해도 될 것 같다.
+// 문자열 변환 문제의 경우 replace를 적극 활용하자.
