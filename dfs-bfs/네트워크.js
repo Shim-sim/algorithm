ceccd209dd6ecddd286f3e42fbebb74ff264787a
ShimSungBo <shimdb96@gmail.com>
2024-03-13 22:00:42 +0900

    add: 네트워크 (재풀이)

--- dfs-bfs/네트워크.js
+++ dfs-bfs/네트워크.js
@@ -8,6 +8,7 @@ function solution(n, computers) {
     while (queue.length) {
       const curNode = queue.shift();
       computers[curNode].forEach((isConnected, idx) => {
+        // 0 이거나 이미 방문했으면 return
         if (!isConnected || visitedNode[idx]) return;
         visitedNode[idx] = true;
         queue.push(idx);
@@ -16,11 +17,17 @@ function solution(n, computers) {
   };
 
   for (let i = 0; i < n; i++) {
+    // 0번 컴퓨터 부터 검사
     if (!visitedNode[i]) {
+      // 아직 방문하지 않았다면
+      bfs(i); // bfs를 통해서 연결되어 있는 컴퓨터들을 방문한다.
       answer++;
-      bfs(i);
     }
   }
 
   return answer;
 }
+
+// 연결되어 있는 컴퓨터들은 bfs를 통해 이미 방문처리가 되었기 때문에
+// 방문처리가 되지 않았다면 다른 컴퓨터 네트워크다.
+// 따라서 bfs를 실행시킨 횟수가 네트워크수가 된다.
